8f0a6d0a41fedca0d856f55bc1972d89ff6e2cf6
Jquarker <jq5827@outlook.com>
2025-02-08 23:12:41 +0800

    map

---
+++ mapping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cmds=(
+	"ros2 launch livox_ros_driver2 msg_MID360_launch.py"
+	"ros2 launch fast_lio mapping.launch.py config_file:=mid360.yaml"
+	)
+
+for cmd in "${cmds[@]}";
+do
+	echo Current CMD : "$cmd"
+	gnome-terminal -- bash -c "cd $(pwd);source ./devel/setup.bash;source install/setup.bash;$cmd;exec bash;"
+	sleep 0.7
+done
