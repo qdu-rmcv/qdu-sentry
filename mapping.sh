1f2048e30df41f492ce52f4239d16fd66ff252e2
Jquarker <jq5827@outlook.com>
2025-02-08 20:41:03 +0800

    map

--- mapping.sh
+++ mapping.sh
@@ -3,7 +3,6 @@
 cmds=(
 	"ros2 launch livox_ros_driver2 msg_MID360_launch.py"
 	"ros2 launch fast_lio mapping.launch.py config_file:=mid360.yaml"
-	"ros2 run octomap_server octomap_saver_node --ros-args -p octomap_path:=src/description/octomap"
 	)
 
 for cmd in "${cmds[@]}";
