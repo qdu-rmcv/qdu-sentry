56014d116bfb983201c9fb55d14b860c058933d7
Jquarker <cjq1822261285@outlook.com>
2025-02-14 12:07:51 +0800

    点云分割实现

--- mapping.sh
+++ mapping.sh
@@ -3,6 +3,7 @@
 cmds=(
 	"ros2 launch livox_ros_driver2 msg_MID360_launch.py"
 	"ros2 launch fast_lio mapping.launch.py config_file:=mid360.yaml"
+	"ros2 launch terrain_analysis terrain_analysis_launch.py"
 	)
 
 for cmd in "${cmds[@]}";
